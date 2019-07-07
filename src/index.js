68366e1b3a11a28c876a6eda74a128e4d93d074b
Clément Le Biez <clement.lebiez@yousign.fr>
2019-07-07 13:10:57 +0200

    fix port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const request = require("request");
 // Creates express app
 const app = express();
 // The port used for Express server
-const PORT = 8080;
+const PORT = process.env.PORT || 3002;
 
 const GITLAB_BASE_URL = process.env.GITLAB_HOST;
 const GITLAB_TOKEN = process.env.GITLAB_TOKEN;
